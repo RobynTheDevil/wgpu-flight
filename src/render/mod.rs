19778627fb6a0e31aac2b7365bc40f459ce82060
RobynTheDevil <fortytwopirates@gmail.com>
2024-04-08 11:10:20 -0400

    move simpletexture to render

--- src/render/mod.rs
+++ src/render/mod.rs
@@ -1,8 +1,4 @@
-use wgpu::{
-    SurfaceError,
-    SurfaceTexture,
-    CommandEncoder,
-};
+use wgpu::*;
 use crate::gpu::Gpu;
 
 pub mod terrain;
@@ -22,6 +18,62 @@ pub struct Vertex {
 }
 impl Vertex {
     pub const fn size_of() -> usize { std::mem::size_of::<Self>() }
+}
+
+//}}}
+
+//{{{ SimpleTexture
+
+pub struct SimpleTexture {
+    pub texture: Texture,
+    pub view: TextureView,
+    pub sampler: Sampler,
+}
+
+impl SimpleTexture {
+    pub const DEPTH_FORMAT: TextureFormat = TextureFormat::Depth32Float;
+
+    pub fn create_depth_texture(device: &Device, config: &SurfaceConfiguration, label: &str) -> Self {
+        let size = Extent3d {
+            width: config.width,
+            height: config.height,
+            depth_or_array_layers: 1,
+        };
+        let desc = TextureDescriptor {
+            label: Some(label),
+            size,
+            mip_level_count: 1,
+            sample_count: 1,
+            dimension: TextureDimension::D2,
+            format: Self::DEPTH_FORMAT,
+            usage: TextureUsages::RENDER_ATTACHMENT
+                | TextureUsages::TEXTURE_BINDING,
+            view_formats: &[],
+        };
+        let texture = device.create_texture(&desc);
+
+        let view = texture.create_view(&TextureViewDescriptor::default());
+        let sampler = device.create_sampler(
+            &SamplerDescriptor {
+                address_mode_u: AddressMode::ClampToEdge,
+                address_mode_v: AddressMode::ClampToEdge,
+                address_mode_w: AddressMode::ClampToEdge,
+                mag_filter: FilterMode::Linear,
+                min_filter: FilterMode::Linear,
+                mipmap_filter: FilterMode::Nearest,
+                compare: Some(CompareFunction::LessEqual),
+                lod_min_clamp: 0.0,
+                lod_max_clamp: 100.0,
+                ..Default::default()
+            }
+        );
+
+        Self {
+            texture,
+            view,
+            sampler,
+        }
+    }
 }
 
 //}}}
